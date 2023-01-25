f83a1e8c49aec598f6b37520bcf1c6f01303c70f
antel460 <antel460@student.liu.se>
2023-01-25 08:37:15 +0100

    import air_lab_common, initial commit lab2

---
+++ air_lab_common/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+import glob
+
+# And modify data_files line so that it looks like this:
+
+
+package_name = 'air_lab_common'
+
+data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + "/screen", glob.glob('screen/*')),
+    ],
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='antel460',
+    maintainer_email='antel460@student.liu.se',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
