b323a36521bea57efcb80c600b31ac800b352853
Matthias Gerdin <matge373@student.liu.se>
2023-02-06 17:01:33 +0100

    navigate_to_pose done

---
+++ air_lab3/air_lab3/DriveToExecutor.py
@@ -0,0 +1,108 @@
+import threading
+import rclpy
+from rclpy.node import Node
+from rclpy.action import ActionClient
+import rclpy.executors
+import math
+
+import TstML
+import TstML.Executor
+
+from nav2_msgs.action import NavigateToPose
+from nav2_msgs.msg import SpeedLimit
+
+import ament_index_python
+
+# Ugly hack to get a new name for each node
+ros_name_counter = 0
+def gen_name(name):
+    global ros_name_counter
+    ros_name_counter += 1
+    return name + str(ros_name_counter)
+
+class DriveToExecutor(TstML.Executor.AbstractNodeExecutor):
+  def __init__(self, node, context):
+    super(TstML.Executor.AbstractNodeExecutor, self).__init__(node,
+          context)
+  
+    self.ros_node = Node(gen_name("driveto_node"))
+    self.publisher_ = self.ros_node.create_publisher(SpeedLimit, '/speed_limit', 10)
+    self._action_client = ActionClient(self.ros_node, NavigateToPose, 'navigate_to_pose')
+    self.executor = rclpy.executors.MultiThreadedExecutor()
+    self.executor.add_node(self.ros_node)
+    self.thread = threading.Thread(target=self.executor.spin)
+    self.thread.start()
+
+  def finalise(self):
+    self.executor.shutdown()
+
+  def start(self):
+    x = float(0)
+    y = float(0)
+    yaw = float(0)
+
+    if self.node().hasParameter(TstML.TSTNode.ParameterType.Specific, "p"):
+      p = self.node().getParameter(TstML.TSTNode.ParameterType.Specific, "p")
+      x = p['x']
+      y = p['y']
+      print("Set some values ", p)
+
+    if self.node().hasParameter(TstML.TSTNode.ParameterType.Specific, "heading"):
+      yaw = self.node().getParameter(TstML.TSTNode.ParameterType.Specific, "heading")
+
+      
+    if self.node().hasParameter(TstML.TSTNode.ParameterType.Specific, "maximum-speed"):
+      sped = self.node().getParameter(TstML.TSTNode.ParameterType.Specific, "maximum-speed")
+      msg = SpeedLimit()
+
+      msg.percentage = False
+      msg.speed_limit = sped
+
+      self.publisher_.publish(msg)
+
+    goal_msg = NavigateToPose.Goal()
+    goal_msg.pose.header.frame_id = "map"
+    goal_msg.pose.pose.position.x = float(x)
+    goal_msg.pose.pose.position.y = float(y)
+    goal_msg.pose.pose.orientation.w = math.cos(yaw/2)
+    goal_msg.pose.pose.orientation.z = math.sin(yaw/2)
+    print("goal_set")
+    #self._action_client.wait_for_server()
+    print("past wait")
+    self._send_goal_future = self._action_client.send_goal_async(goal_msg, self.feedback_callback)
+    print("past send goal_async")
+    self._send_goal_future.add_done_callback(self.goal_response_callback)
+
+    print("past send goal_callback")
+
+    return TstML.Executor.ExecutionStatus.Started()
+
+  def feedback_callback(self, feedback_msg):
+    print(feedback_msg.feedback)
+
+  def goal_response_callback(self, future):
+    self._goal_handle = future.result()
+    if not self._goal_handle.accepted:
+      self.executionFinished(TstML.Executor.ExecutionStatus.Aborted())
+      self.ros_node.get_logger().error('Goal rejected :(')
+    else:
+      self.ros_node.get_logger().error('Goal accepted :)')
+
+      self._get_result_future = self._goal_handle.get_result_async()
+      self._get_result_future.add_done_callback(self.handle_result_callback)
+
+  def handle_result_callback(self, future):
+    print("Finished!")
+    self.executionFinished(TstML.Executor.ExecutionStatus.Finished())
+
+  def pause(self):
+    self.ros_node.get_logger().info('Pause is not possible.')
+    return TstML.Executor.ExecutionStatus.Running()
+  def resume(self):
+    return TstML.Executor.ExecutionStatus.Running()
+  def stop(self):
+    self._goal_handle.cancel_goal()
+    return TstML.Executor.ExecutionStatus.Finished()
+  def abort(self):
+    self._goal_handle.cancel_goal()
+    return TstML.Executor.ExecutionStatus.Aborted()
--- air_lab3/air_lab3/lab3_node.py
+++ air_lab3/air_lab3/lab3_node.py
@@ -1,47 +1,66 @@
 from air_lab_interfaces.srv import ExecuteTst
+import std_srvs.srv
 
+from .UndockExecutor import UndockExecutor
+from .DockExecutor import DockExecutor
+from .DriveToExecutor import DriveToExecutor
 import rclpy
 from rclpy.node import Node
 import TstML
 import ament_index_python
 import TstML.Executor
+from rclpy.callback_groups import ReentrantCallbackGroup
 
+std_srvs.srv.Empty
 
 class MinimalService(Node):
 
     def __init__(self):
         super().__init__('minimal_service')
-        print("Hi Heloo")
         self.srv = self.create_service(ExecuteTst, 'execute_tst', self.execute_tst_callback)
-        tst_registry = TstML.TSTNodeModelsRegistry()
-        tst_registry.loadDirectory(ament_index_python.get_package_prefix("air_tst") +  "/share/air_tst/configs")
-        
+        self.group = ReentrantCallbackGroup()
+        service = self.create_service(std_srvs.srv.Empty, 'abort', self.callback_abort, callback_group=self.group)
+        service = self.create_service(std_srvs.srv.Empty, 'stop', self.callback_stop, callback_group=self.group)
+        service = self.create_service(std_srvs.srv.Empty, 'pause', self.callback_pause, callback_group=self.group)
+        service = self.create_service(std_srvs.srv.Empty, 'resume', self.callback_resume, callback_group=self.group)
+        self.tst_registry = TstML.TSTNodeModelsRegistry()
+        self.tst_registry.loadDirectory(ament_index_python.get_package_prefix("air_tst") +  "/share/air_tst/configs")
+
+        # Create a registry with node executors
+        self.tst_executor_registry = TstML.Executor.NodeExecutorRegistry()
+
+        # Setup the executors for sequence and concurrent
+        self.tst_executor_registry.registerNodeExecutor(
+            self.tst_registry.model("seq"),
+            TstML.Executor.DefaultNodeExecutor.Sequence)
+        self.tst_executor_registry.registerNodeExecutor(
+            self.tst_registry.model("conc"),
+            TstML.Executor.DefaultNodeExecutor.Concurrent)
+        self.tst_executor_registry.registerNodeExecutor(
+            self.tst_registry.model("undock"),
+            UndockExecutor)
+        self.tst_executor_registry.registerNodeExecutor(
+            self.tst_registry.model("dock"),
+            DockExecutor)
+        self.tst_executor_registry.registerNodeExecutor(
+            self.tst_registry.model("drive-to"),
+            DriveToExecutor)
 
 
     def execute_tst_callback(self, request, response):
         filename = request.tst_file
-        tst_node = TstML.TSTNode.load(filename, tst_registry)
-        # Create a registry with node executors
-        tst_executor_registry = TstML.Executor.NodeExecutorRegistry()
+        tst_node = TstML.TSTNode.load(filename, self.tst_registry)
 
-        # Setup the executors for sequence and concurrent
-        tst_executor_registry.registerNodeExecutor(
-            tst_registry.model("seq"),
-            TstML.Executor.DefaultNodeExecutor.Sequence)
-        tst_executor_registry.registerNodeExecutor(
-            tst_registry.model("conc"),
-            TstML.Executor.DefaultNodeExecutor.Concurrent)
-
-            # Create an executor using the executors defined
+        # Create an executor using the executors defined
         # in tst_executor_registry
-        tst_executor = TstML.Executor.Executor(tst_node,
-            tst_executor_registry)
+        self.tst_executor = TstML.Executor.Executor(tst_node,
+            self.tst_executor_registry)
 
         # Start execution
-        tst_executor.start()
+        self.tst_executor.start()
 
         # Block until the execution has finished
-        status = tst_executor.waitForFinished()
+        status = self.tst_executor.waitForFinished()
 
         # Display the result of execution
         if status.type() == TstML.Executor.ExecutionStatus.Type.Finished:
@@ -52,18 +71,40 @@ class MinimalService(Node):
         else:
             response.success = False
 
+        return response
 
-        print("Hello Hi")
+    def callback_abort(self, request, response):
+        print(self.tst_executor.abort())
+        return response
+
+    def callback_stop(self, request, response):
+        print(self.tst_executor.stop())
+        return response
+
+    def callback_pause(self, request, response):
+        print(self.tst_executor.pause())
+        return response
+
+    def callback_resume(self, request, response):
+        print(self.tst_executor.resume())
         return response
 
 
 def main():
-    print("Hi from mainfrmae")
     rclpy.init()
 
     minimal_service = MinimalService()
 
-    rclpy.spin(minimal_service)
+    group = ReentrantCallbackGroup()
+
+    executor = rclpy.executors.MultiThreadedExecutor()
+    executor.add_node(minimal_service)
+    executor.spin()
+
+
+
+
+    #rclpy.spin(minimal_service)
 
     rclpy.shutdown()
 
